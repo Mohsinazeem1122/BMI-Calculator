4e5a690973e30841d365a6a396422937aabce06c
Mohsinazeem1122 <mohsincodyex11@gmail.com>
2024-10-11 16:06:17 +0500

    creating function for bmi

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const weightInput = document.getElementById("weight");
 const heightInput = document.getElementById("height");
 const result = document.getElementById("result");
 const resultStatement = document.getElementById("result-statement");
+
+function calculateBMI() {}
