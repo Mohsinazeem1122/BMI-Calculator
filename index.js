f5083bf8453371f949861dd04e249079ab69c485
Mohsinazeem1122 <mohsincodyex11@gmail.com>
2024-10-11 17:11:29 +0500

    category statement functionality done

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ function calculateBMI() {
   let height = heightInput.value;
   let BMI = (weight / (height / 100) ** 2).toFixed(2);
   result.innerText = BMI;
+
+  if (!weight || !height) {
+    return alert("Please enter your weight and height");
+  }
+
+  if (BMI >= 40.0) {
+    resultStatement.innerText = "You are obese";
+  } else if (BMI >= 25.0 && BMI < 40) {
+    resultStatement.innerText = "You are overweight";
+  } else if (BMI >= 18.5 && BMI < 25) {
+    resultStatement.innerText("You are normal");
+  } else {
+    resultStatement.innerText("You are underweight");
+  }
 }
